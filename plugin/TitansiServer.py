28f3ffb2d2e9ea00450d85792270f871e06bacfa
Gareth Griffiths <gareth@synbyte.com>
2017-10-12 23:37:46 +0100

    start of TitansiServer

--- plugin/TitansiServer.py
+++ plugin/TitansiServer.py
@@ -1,10 +1,26 @@
 import sys
-import vim
 import os
 import json
 import socket
+from pprint import pprint
 
 class TitansiServer:
     def __init__(self):
 
-    def start(self):
+        with open('config.json') as data_file:
+            data = json.load(data_file)
+
+        s = socket.socket()
+        s.bind((data["server"]["host"], data["server"]["port"]))
+
+        while True:
+            s.listen(1)
+            conn, addr = s.accept()
+            print ("Connection from: " + str(addr))
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+            print ("from connected  user: " + str(data))
+        conn.close()
+
+TitansiServer()
